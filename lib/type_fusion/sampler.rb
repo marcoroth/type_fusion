3715ab5a373adddcc21cfa78e7f3633648c27f70
Marco Roth <marco.roth@intergga.ch>
2023-08-25 15:45:01 +0200

    Update Struct Syntax for Ruby 3.2
    Seems like kwargs are not really supported anymore...

--- lib/type_fusion/sampler.rb
+++ lib/type_fusion/sampler.rb
@@ -37,7 +37,7 @@ module TypeFusion
           parameters = extract_parameters(args, tracepoint.binding)
           return_value = type_for_object(tracepoint.return_value)
 
-          sample = SampleCall.new({
+          sample_values = {
             gem_name: gem,
             gem_version: version,
             receiver: receiver,
@@ -47,7 +47,9 @@ module TypeFusion
             parameters: parameters,
             application_name: TypeFusion.config.application_name,
             return_value: return_value,
-          })
+          }
+
+          sample = SampleCall.new(sample_values.values)
 
           samples << sample
 
