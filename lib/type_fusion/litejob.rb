37a0efc4bf4b249488767d473188361683b55a3d
Marco Roth <marco.roth@intergga.ch>
2023-08-25 03:42:40 +0200

    Silence LiteJob logger for now

--- lib/type_fusion/litejob.rb
+++ lib/type_fusion/litejob.rb
@@ -5,3 +5,10 @@ require "litejob"
 # Litequeue.configure do |config|
 #   config.path = "db/type_fusion/queue.sqlite3"
 # end
+
+Litejob.configure do |config|
+  config.logger = Class.new do
+    def self.info(*args)
+    end
+  end
+end
